611ed6f573f57048a8e52b95a71ba936484aa90c
Harry Friend <harryfriend@hotmail.co.uk>
2014-10-08 21:27:32 +0100

    Update to Sprite Class

--- SDL/Sprite.cpp
+++ SDL/Sprite.cpp
@@ -1,2 +1,42 @@
 #include "Sprite.h"
+Sprite::Sprite(int x, int y)
+{
+	BoundBoxX = x;
+	BoundBoxY = y;
+}
+
+Sprite::~Sprite()
+{
+
+}
+
+void Sprite::updateSpritePosition()
+{
+
+}
+
+void Sprite::drawSpritePosition()
+{
+
+}
+
+int Sprite::GetBoundBoxX(void)
+{
+
+}
+
+int Sprite::GetBoundBoxY(void)
+{
+
+}
+
+int Sprite::GetBoundBoxW(void)
+{
+
+}
+
+int Sprite::GetBoundBoxH(void)
+{
+
+}
 
--- SDL/Sprite.h
+++ SDL/Sprite.h
@@ -2,9 +2,17 @@
 class Sprite : protected Texture
 {
 public:
-	Sprite();
+	Sprite(int x, int y);
 	~Sprite();
-	void updatePlayerPosition();
-	void drawPlayerPosition();
-private:
+	void updateSpritePosition();
+	void drawSpritePosition();
+	int GetBoundBoxX(void);
+	int GetBoundBoxY(void);
+	int GetBoundBoxW(void);
+	int GetBoundBoxH(void);
+protected:
+	int BoundBoxX;
+	int BoundBoxY;
+	int BoundBoxW;
+	int BoundBoxH;
 };
