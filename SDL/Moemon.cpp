0fffa2fd4b962ba655895b1b4bd24c4f67e89e93
Harry Friend <harryfriend@hotmail.co.uk>
2014-11-27 08:50:17 +0000

    Updated MoeMon.txt
    *Added new values for MoeMon.txt to read in.
    *Base Health,attack,defense and other stats will be rewritten as the
    current values are just placeholders

--- SDL/Moemon.cpp
+++ SDL/Moemon.cpp
@@ -1,12 +1,18 @@
 #include "Moemon.h"
 
-Moemon::Moemon(int id, int health, int level, std::string name, std::string type) : Entity()
+Moemon::Moemon(int id, int health, int attack, int defense, int SpAtk, int SpDef, int Speed, int level, std::string name, std::string type) : Entity()
 {
 	i_ID = id;
-	i_Health = health;
+	i_Health = health;	
+	i_Attack = attack;
+	i_Defense = defense;
+	i_SpAtk = SpAtk;
+	i_SpDef = SpDef;
+	i_Speed = Speed;
 	i_Level = level;
 	s_MoeMonName = name;
 	s_Type = type;
+
 	SDL_Surface* EntitySurface = NULL;
 	SDL_Texture* EntityTexture = NULL;
 }
--- SDL/Moemon.h
+++ SDL/Moemon.h
@@ -6,13 +6,18 @@
 class Moemon : public Entity
 {
 public:
-	Moemon(int id, int health, int level, std::string name, std::string type);
+	Moemon(int id, int health, int attack, int defense, int SpAtk, int SpDef, int Speed, int level, std::string name, std::string type);
 	
 private:
 
 	int i_ID;
 	int i_Health;
 	int i_Level;
+	int i_Attack;
+	int i_Defense;
+	int i_SpAtk;
+	int i_SpDef;
+	int i_Speed;
 	std::string s_MoeMonName;
 	std::string s_Type;
 
